fdbc306c5b06028d4d5154d8c19ae0683a5d0493
Saari <e2202896@edu.vamk.fi>
2024-11-11 13:07:41 +0200

    CSS moduulit

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,9 @@
-// import React from 'react';
-import styled from 'styled-components';
+ import React from 'react';
+
+ import styles from './Button.module.css';
 
 //import './Button.css';
-const Button = styled.button`
+/*const Button = styled.button`
 width: 100%:
   font: inherit;
   padding: 0.5rem 1.5rem;
@@ -25,7 +26,14 @@ width: 100%:
   border-color: #ac0e77;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
 }
-  `;
+  `; */
+  const Button = props => {
+    return (
+      <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+      </button>
+    );
+  };
  
 
 export default Button;
